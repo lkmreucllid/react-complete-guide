ac1bfea62ff4483bcb0a04d71928f4fe9d9b15be
lkmreucllid <lkmreucllid01@gmail.com>
2021-02-08 00:44:58 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, i am a React App</h1>
-    //   </div>
-    // );
+    return (
+      <div className="App">
+        <h1>Hi, i am a React App</h1>
+        <p>This is actually Working!</p>
+        <Person />
+      </div>
+    );
     //return React.createElement('div',null,'h1','Hi i\'m the new React App');
-    return React.createElement('div',{className:'App'},React.createElement('h1',null,'Does this works now'));
+    //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Does this works now'));
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Person = () =>{
+    return <p>this is the person funcion created manually</p>
+}
+
+export default Person;
