06b9f726b3dd829cb302317928dc8a541c470f26
lkmreucllid <lkmreucllid01@gmail.com>
2021-02-08 01:35:38 +0530

    Understandng Children Properties and State and Using State

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name:'Max Tennyson', age: 58},
+      {name:'Ben Tennyson', age: 10},
+      {name:'Gwen Tennyson', age: 11}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, i am a React App</h1>
         <p>This is actually Working!</p>
-        <Person name="Max Tennyson" age="10"/>
-        <Person name="Gwen Tennyson" age="11"/>
-        <Person name="Max Tennyson" age="58"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies are reading and sleeping</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div',null,'h1','Hi i\'m the new React App');
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Person = (props) =>{
-    return <p>Hi, my name is {props.name} and i am {props.age} years old!</p>
+    return(
+    <div>
+        <p>Hi, my name is {props.name} and i am {props.age} years old!</p>
+        <p>{props.children}</p>
+    </div>
+    );
 }
 
 export default Person;
